aea546821898dca74df5e3b3ffbb517426ab728e
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-14 22:47:45 +0300

    exercises

--- exercise1.py
+++ exercise1.py
@@ -12,8 +12,35 @@ class Circle:
 
 
 circleOne=Circle(4)
-print(circleOne.circumfrence()) 
+# print(circleOne.circumfrence()) 
 circleTwo=Circle(10) 
-print(circleTwo.area())   
+# print(circleTwo.area())   
+
+
+class Rectangle:
+      
+      def __init__(self,length,width):
+            self.length=length
+            self.width=width
+      def findArea(self):
+            return self.length*self.width
+      
+
+rectangleOne=Rectangle(4,5)
+print(f" Area of a rectangle is {rectangleOne.findArea()}")     
+
+class SimpleInterest:
+      def __init__(self,principle,rate,time):
+            self.principle=principle
+            self.rate = rate
+            self.time=time
+
+      def calcSimpleInterest(self):
+            return ( (self.principle*self.rate*self.time)/100)
+      
+
+client=SimpleInterest(10000,5,2)
+print(f"Your simple intrest is {client.calcSimpleInterest()}")      
+
 
 
